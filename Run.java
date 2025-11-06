5360ddeb12613cf23e23928d93d42de0f55b8960
Pedro Garcia <108310232+pedrogarciia@users.noreply.github.com>
2025-11-06 19:41:24 -0300

    Create Run.java

---
+++ Run.java
@@ -0,0 +1,16 @@
+public class Run {
+    private Jogador jogador;
+    private int nivelAtual;
+
+    public void iniciar() {
+        // Cria jogador, inicia primeira partida
+    }
+
+    public void proximaPartida() {
+        // Cria bot mais forte e começa nova partida
+    }
+
+    public void reiniciar() {
+        // Resetar tudo se o jogador perder
+    }
+}
