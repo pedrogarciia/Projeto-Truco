fc3333d3ef755345deaf71c2df9159300736feb2
Pedro Garcia <108310232+pedrogarciia@users.noreply.github.com>
2025-11-08 19:34:07 -0300

    Create Personalidade.java

---
+++ Personalidade.java
@@ -0,0 +1,7 @@
+public enum Personalidade {
+    VALENTE,      // Pede truco sem olhar as cartas
+    MEDROSO,      // Só pede truco quando tem manilha
+    DOIDO,        // Pede truco e aumenta mesmo sem ter nada
+    MALANDRO,     // Pede truco em bons momentos, mesmo com cartas medianas
+    CALCULISTA    // Avalia bem a mão antes de pedir ou aceitar
+}
